c457bfc3497e4aea5738d83e33e8068be66d66cb
Alicia Sandoval <asandovalgarcia@csumb.edu>
2019-10-24 20:12:34 -0700

    lab6

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render("index.html");
+});
+
+app.get("/mercury", function(req, res){
+    res.render("mercury.html");
+});
+
+app.get("/venus", function(req, res){
+    res.render("venus.html");
+});
+
+app.get("/earth", function(req, res){
+    res.render("earth.html");
+});
+
+app.get("/", function (req, res) {
+    res.send("it works!");
+});
+
+// app.get("/mercury", function (req, res) {
+//     res.send("This will be Mercury web page!");
+// });
+
+// app.get("/venus", function (req, res) {
+//     res.send("This will be Venus web page!");
+// });
+
+// server listener
+app.listen("8081", "0.0.0.0", function () {
+    console.log("Express Server is Running...")
+});
+
+//heroku
+app.listen(process.env.PORT, process.env.IP, function () {
+    console.log("Running Express Server...");
+});
